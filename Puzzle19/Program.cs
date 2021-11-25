97ba2bb3c684b2ef437baaae1a114ede54f64b0e
Ilia Opiakin <i.opyakin@outlook.com>
2021-11-25 22:28:13 +0100

    It works..accidentally *nned to find the issue)

--- Puzzle19/Program.cs
+++ Puzzle19/Program.cs
@@ -7,13 +7,15 @@ namespace Puzzle19
 {
     class Program
     {
+        // data_1.txt 239 & 405
+        static StreamReader file = new StreamReader(@"C:\Users\iopya\Source\Repos\AOC2020\Puzzle19\data_1.txt");
+        static List<string> Examples = new List<string>();
         static Dictionary<int, string> Rules = new Dictionary<int, string>();
+        static int[] matchedCase;
         static void Main(string[] args)
         {
-            StreamReader file = new StreamReader(@"C:\Users\iopya\Source\Repos\AOC2020\Puzzle19\data.txt");
             string line = file.ReadLine();
-            List<string> Examples = new List<string>();
-            
+
             // Parsing the rules
             while (line != "")
             {
@@ -30,6 +32,70 @@ namespace Puzzle19
                     Examples.Add(line);
                 line = file.ReadLine();
             }
+            
+            matchedCase = new int[Examples.Count];
+            for (int i = 0; i < Examples.Count; i++)
+                matchedCase[i] = 0;
+
+            SolveThePuzzle(1);
+
+            for (int i = 0; i < Examples.Count; i++)
+                matchedCase[i] = 0;
+
+            SolveThePuzzle(2);
+
+        }
+        static void SolveThePuzzle(int part=1)
+        {
+            if (part == 1)
+            {
+                Console.WriteLine("Part One");
+                int res = GetCasesForPartOne();
+                Console.WriteLine(res);
+                Console.WriteLine("");
+
+            }
+            else if (part == 2)
+            {
+                Console.WriteLine("Part Two");
+                Rules.Remove(8);
+                //Rules.Add(8, "42 | 42 8");
+                Rules.Add(8, "42 | 42 *");
+
+                Rules.Remove(11);
+                //Rules.Add(11, "42 31 | 42 11 31");
+                Rules.Add(11, "42 31 | 42 * 31");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    int x = GetCasesForPartTwo();
+                    Console.WriteLine(x);
+                }
+
+            }
+            else
+                return;
+
+        }
+
+        static int GetCasesForPartOne()
+        {
+            string simpleRule;
+            Rules.TryGetValue(0, out simpleRule);
+            simpleRule = " " + simpleRule + " ";
+
+            bool bRuleIsComplex = true;
+            while (bRuleIsComplex)
+                bRuleIsComplex = SimplifyRules(simpleRule, out simpleRule);
+
+            int matchedCount = GetMatchedCases(simpleRule);
+
+            return matchedCount;
+        }
+
+        static int GetCasesForPartTwo()
+        {
+            int res = 0;
 
             string simpleRule;
             Rules.TryGetValue(0, out simpleRule);
@@ -37,29 +103,55 @@ namespace Puzzle19
 
             bool bRuleIsComplex = true;
             while (bRuleIsComplex)
-                bRuleIsComplex = simplifyRules(simpleRule, out simpleRule);
+                bRuleIsComplex = SimplifyRules(simpleRule, out simpleRule);
 
+            string rule8;
+            Rules.TryGetValue(8, out rule8);
+            rule8 = rule8.Replace("*", rule8);
+            Rules.Remove(8);
+            Rules.Add(8, rule8);
 
+            string rule11;
+            Rules.TryGetValue(11, out rule11);
+            rule11 = rule11.Replace("*", rule11);
+            Rules.Remove(11);
+            Rules.Add(11, rule11);
+
+            GetMatchedCases(simpleRule);
+
+            for (int i = 0; i < Examples.Count; i++)
+            {
+                if (matchedCase[i] > 0)
+                    res++;
+            }
+
+            return res;
+        }
+
+        static int GetMatchedCases(string simpleRule)
+        {
             simpleRule = "^" + simpleRule.Replace(" ", "") + "$";
             //Console.WriteLine(simpleRule);
 
             Regex rg = new Regex(simpleRule);
-            int matchedCount=0;
+            int matchedCount = 0;
+            int caseN = 0;
 
             foreach (string Example in Examples)
             {
                 if (rg.IsMatch(Example))
                 {
-                    // Console.WriteLine(Example);
+                    //Console.WriteLine(Example);
                     matchedCount++;
+                    matchedCase[caseN]++;
                 }
+                caseN++;
             }
-            Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("The answer: {0}", matchedCount);
 
+            return matchedCount;
         }
-        
-        static bool simplifyRules(string simpleRule, out string result)
+
+        static bool SimplifyRules(string simpleRule, out string result)
         {
             string tempRule = null;
 
@@ -67,7 +159,7 @@ namespace Puzzle19
 
             foreach (string r in firstRule)
             {
-                if(r!="a" && r!="b" && r!="(" && r!= ")" && r!="|" && r!= "")
+                if(r!="a" && r!="b" && r!="(" && r!= ")" && r!="|" && r!= "" && r != "*")
                 {
                     Rules.TryGetValue(int.Parse(r), out tempRule);
                     if (tempRule == null)
