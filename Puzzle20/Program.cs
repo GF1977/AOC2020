df934f6d5943fd3e050f061304a91caa438e238c
Ilya Opyakin <i.opyakin@outlook.com>
2022-03-23 22:43:11 +0100

    Added rotation function and flip

--- Puzzle20/Program.cs
+++ Puzzle20/Program.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 
 namespace Puzzle20
 {
-    public struct EdgesHash {public int N; public int E; public int S; public int W; }
+    public struct EdgesHash {public string N; public string E; public string S; public string W; }
     class Tile
     {
         private int id;
@@ -28,9 +28,58 @@ namespace Puzzle20
             Hash.W = GetEdge("W");
 
             edgesHashes.Add(Hash);
+
+            GetRotatedEdges();
+
+        }
+        public void GetRotatedEdges()
+        {
+            List<EdgesHash> edgesHashesTMP = new List<EdgesHash>();
+            
+            foreach (EdgesHash EH in edgesHashes)
+            {
+                EdgesHash[] HashRotated = new EdgesHash[3];
+
+                HashRotated[0] = RotateRightOnce(EH);
+                HashRotated[1] = RotateRightOnce(HashRotated[0]);
+                HashRotated[2] = RotateRightOnce(HashRotated[1]);
+
+                foreach (EdgesHash EHrotated in HashRotated)
+                    edgesHashesTMP.Add(EHrotated);
+            }
+
+            edgesHashes.AddRange(edgesHashesTMP);
         }
 
-        public int GetEdge(string Side)
+        public string InverseHash(string Hash)
+        { 
+            char[] HashChars = Hash.ToCharArray();
+            Array.Reverse(HashChars);
+            return HashChars.ToString();
+        }
+
+        public EdgesHash FlipX(EdgesHash EH)
+        {
+            EdgesHash EHtmp;
+            EHtmp.N = InverseHash(EH.S); EHtmp.E = InverseHash(EH.E); EHtmp.S = InverseHash(EH.N); EHtmp.W = InverseHash(EH.W);
+            return EHtmp;
+        }
+
+        public EdgesHash FlipY(EdgesHash EH)
+        {
+            EdgesHash EHtmp;
+            EHtmp.N = InverseHash(EH.N); EHtmp.E = InverseHash(EH.W); EHtmp.S = InverseHash(EH.S); EHtmp.W = InverseHash(EH.E);
+            return EHtmp;
+        }
+
+        public EdgesHash RotateRightOnce(EdgesHash EH)
+        {
+            EdgesHash EHtmp;
+            EHtmp.N = EH.W; EHtmp.E = EH.N; EHtmp.S = EH.E; EHtmp.W = EH.S;
+            return EHtmp;
+        }
+
+        public string GetEdge(string Side)
         {
             (int X, int Y) Start    = (0,0);
             (int dX, int dY) D      = (0,0);
@@ -43,13 +92,10 @@ namespace Puzzle20
                 case "W": Start = (0, 9); D = ( 0, -1); break;
             }
 
-            int nRes = 0;
+            string nRes = String.Empty;
             for(int i = 0; i < 10;i++)
                 {
-                    nRes *= 2;
-                    if (Photo[Start.Y, Start.X] == '#')
-                        nRes += 1;
-
+                    nRes += Photo[Start.Y, Start.X];
                     Start.X += D.dX;
                     Start.Y += D.dY;
                 }
