13c58e7c22aa61cfe67ce6a7547e508d736f63a5
Ilya Opyakin <i.opyakin@outlook.com>
2022-03-27 11:25:27 +0200

    Some refactoring of part one

--- Puzzle20/Program.cs
+++ Puzzle20/Program.cs
@@ -5,22 +5,30 @@ using System.Text.RegularExpressions;
 
 namespace Puzzle20
 {
-    public struct EdgesHash {public string N; public string E; public string S; public string W; }
+    public struct EdgesHash
+    {
+        public string N; public string E; public string S; public string W;
+        public EdgesHash (string N, string E, string S, string W)
+            {
+                this.N = N; this.E = E; this.S = S; this.W = W;
+            }
+    }
     class Tile
     {
         public int id;
-        public Dictionary<int, int> connections;
-        private char[,] Photo = new char[10, 10];
+        public Dictionary<int, string> connections;
+        private char[,] Photo;
         public List<EdgesHash> edgesHashes = new List<EdgesHash>();
-        public Tile(string RawData)
+        public Tile(string RawData, int nPhotoSize = 10)
         {
-            connections = new Dictionary<int, int>();
+            Photo = new char[nPhotoSize, nPhotoSize];
+            connections = new Dictionary<int, string>();
             string[] parts = RawData.Split("\r\n");
             string[] sId = parts[0].Split(" ");
             this.id = int.Parse(sId[1].Replace(":",""));
 
-            for(int nRow = 0; nRow < parts.Length - 1; nRow++)
-                for (int nCol = 0; nCol < 10; nCol++)
+            for(int nRow = 0; nRow < nPhotoSize; nRow++)
+                for (int nCol = 0; nCol < nPhotoSize; nCol++)
                     Photo[nRow, nCol] = parts[nRow + 1][nCol];
 
             EdgesHash Hash;
@@ -32,48 +40,37 @@ namespace Puzzle20
             edgesHashes.Add(Hash);
 
             GetRotatedEdges();
-
         }
         public void GetRotatedEdges()
         {
             List<EdgesHash> edgesHashesTMP = new List<EdgesHash>();
-            
             foreach (EdgesHash EH in edgesHashes)
             {
-                EdgesHash[] HashRotated = new EdgesHash[3];
+                edgesHashesTMP.AddRange(Get4RotatedEdges(EH, flipType:"none"));
+                edgesHashesTMP.AddRange(Get4RotatedEdges(EH, flipType:"X"));
+                edgesHashesTMP.AddRange(Get4RotatedEdges(EH, flipType:"Y"));
+            }
+            edgesHashes.AddRange(edgesHashesTMP);
+        }
 
-                HashRotated[0] = RotateRightOnce(EH);
-                HashRotated[1] = RotateRightOnce(HashRotated[0]);
-                HashRotated[2] = RotateRightOnce(HashRotated[1]);
+        private List<EdgesHash> Get4RotatedEdges(EdgesHash EH, string flipType = "none")
+        {
+            List<EdgesHash> edgesHashesTMP = new List<EdgesHash>();
+            EdgesHash EHtmp = EH;
+            int n = 4;
 
-                foreach (EdgesHash EHrotated in HashRotated)
-                    edgesHashesTMP.Add(EHrotated);
+            if (flipType == "none") 
+                n = 3; // "none" means no flip, rotation only. We need only 3 rotations, as the rotation 4th is the original EH (rotation 0)
+            else
+                EHtmp = RotateRightOnce(Flip(EH, flipType));
 
-
-                HashRotated = new EdgesHash[4];
-
-                HashRotated[0] = RotateRightOnce(FlipX(EH));
-                HashRotated[1] = RotateRightOnce(HashRotated[0]);
-                HashRotated[2] = RotateRightOnce(HashRotated[1]);
-                HashRotated[3] = RotateRightOnce(HashRotated[2]);
-
-                foreach (EdgesHash EHrotated in HashRotated)
-                    edgesHashesTMP.Add(EHrotated);
-
-
-                HashRotated = new EdgesHash[4];
-
-                HashRotated[0] = RotateRightOnce(FlipY(EH));
-                HashRotated[1] = RotateRightOnce(HashRotated[0]);
-                HashRotated[2] = RotateRightOnce(HashRotated[1]);
-                HashRotated[3] = RotateRightOnce(HashRotated[2]);
-
-                foreach (EdgesHash EHrotated in HashRotated)
-                    edgesHashesTMP.Add(EHrotated);
-
+            for (int i= 0; i < n; i++)
+            {
+                edgesHashesTMP.Add(EHtmp);
+                EHtmp = RotateRightOnce(EHtmp);
             }
 
-            edgesHashes.AddRange(edgesHashesTMP);
+            return edgesHashesTMP;
         }
 
         public string InverseHash(string Hash)
@@ -83,31 +80,24 @@ namespace Puzzle20
             return  new string(HashChars);
         }
 
-        public EdgesHash FlipX(EdgesHash EH)
+        public EdgesHash Flip(EdgesHash EH, string flip)
         {
-            EdgesHash EHtmp;
-            EHtmp.N = InverseHash(EH.S); EHtmp.E = InverseHash(EH.E); EHtmp.S = InverseHash(EH.N); EHtmp.W = InverseHash(EH.W);
+            EdgesHash EHtmp = new EdgesHash(InverseHash(EH.N), InverseHash(EH.W), InverseHash(EH.S), InverseHash(EH.E));
+            if (flip == "X")
+            {
+                EHtmp = new EdgesHash(InverseHash(EH.S), InverseHash(EH.E), InverseHash(EH.N), InverseHash(EH.W));
+            }
+           
             return EHtmp;
         }
-
-        public EdgesHash FlipY(EdgesHash EH)
-        {
-            EdgesHash EHtmp;
-            EHtmp.N = InverseHash(EH.N); EHtmp.E = InverseHash(EH.W); EHtmp.S = InverseHash(EH.S); EHtmp.W = InverseHash(EH.E);
-            return EHtmp;
-        }
-
         public EdgesHash RotateRightOnce(EdgesHash EH)
         {
-            EdgesHash EHtmp;
-            EHtmp.N = EH.W; EHtmp.E = EH.N; EHtmp.S = EH.E; EHtmp.W = EH.S;
-            return EHtmp;
+            return new EdgesHash(EH.W, EH.N, EH.E, EH.S);
         }
-
         public string GetEdge(string Side)
         {
-            (int X, int Y) Start    = (0,0);
-            (int dX, int dY) D      = (0,0);
+            (int  X, int  Y) Start      = (0,0);
+            (int dX, int dY) D          = (0,0);
 
             switch (Side)
             {
@@ -124,21 +114,18 @@ namespace Puzzle20
                     Start.X += D.dX;
                     Start.Y += D.dY;
                 }
-
             return nRes;
         }
     }
     class Program
     {
-        
+        // Answers:         Part one = 66020135789767   
         static List<Tile> Tiles = new List<Tile>();
+        static Dictionary<string, int> Edges = new Dictionary<string, int>();
         static void Main(string[] args)
         {
-
-
             // parsing
             ParsingInputData(@"..\..\..\data_p.txt");
-
             CheckingEdges();
 
             long res = 1;
@@ -146,75 +133,41 @@ namespace Puzzle20
                 if (tile.connections.Count == 2)
                     res *= tile.id;
 
-                Console.WriteLine("Part one: {0, 10:0}", res);
+            Console.WriteLine("Part one: {0, 10:0}", res);
 
-
+            if (res != 66020135789767)
+                Console.WriteLine("Error!");
         }
-
         private static void CheckingEdges()
         {
-            Dictionary<string, int> Edges = new Dictionary<string, int>();
-
             foreach (Tile tile in Tiles)
             {
-                int n = 0;
                 foreach (EdgesHash EH in tile.edgesHashes)
                 {
-                    bool b = Edges.TryAdd(EH.N, tile.id);
-                    if (!b && tile.id != Edges[EH.N])
-                    {
-                        tile.connections.TryAdd(Edges[EH.N], 0);
-                        Tile T = Tiles.Find(t => t.id == Edges[EH.N]);
-                        T.connections.TryAdd(tile.id, 0);
-                    }
-                        //Console.WriteLine("Tile: {0}  &   Tile: {1}", tile.id, Edges[EH.N]);
-
-                    b = Edges.TryAdd(EH.E, tile.id);
-                    if (!b && tile.id != Edges[EH.E])
-                    {
-                        tile.connections.TryAdd(Edges[EH.E], 0);
-                        Tile T = Tiles.Find(t => t.id == Edges[EH.E]);
-                        T.connections.TryAdd(tile.id, 0);
-                    }
-                    //tile.connections.TryAdd(Edges[EH.E].Substring(0, 4), "");
-                    //Console.WriteLine("Tile: {0}  &   Tile: {1}", tile.id, Edges[EH.E]);
-
-                    b = Edges.TryAdd(EH.S, tile.id);
-                    if (!b && tile.id != Edges[EH.S])
-                    {
-                        tile.connections.TryAdd(Edges[EH.S], 0);
-                        Tile T = Tiles.Find(t => t.id == Edges[EH.S]);
-                        T.connections.TryAdd(tile.id, 0);
-                    }
-                    //    tile.connections.TryAdd(Edges[EH.S].Substring(0, 4), "");
-                    //Console.WriteLine("Tile: {0}  &   Tile: {1}", tile.id, Edges[EH.S]);
-
-
-                    b = Edges.TryAdd(EH.W, tile.id);
-                    if (!b && tile.id != Edges[EH.W])
-                    {
-                        tile.connections.TryAdd(Edges[EH.W], 0);
-                        Tile T = Tiles.Find(t => t.id == Edges[EH.W]);
-                        T.connections.TryAdd(tile.id, 0);
-                    }
-                    //    tile.connections.TryAdd(Edges[EH.W].Substring(0, 4), "");
-                    //Console.WriteLine("Tile: {0}  &   Tile: {1}", tile.id, Edges[EH.W]);
-
-                    n++;
+                    AddMatchToDict(EH.N, tile);
+                    AddMatchToDict(EH.E, tile);
+                    AddMatchToDict(EH.S, tile);
+                    AddMatchToDict(EH.W, tile);
                 }
             }
         }
-
+        private static void AddMatchToDict(string n, Tile tile)
+        {
+            bool b = Edges.TryAdd(n, tile.id);
+            if (!b && tile.id != Edges[n])
+            {
+                tile.connections.TryAdd(Edges[n], n);
+                Tile T = Tiles.Find(t => t.id == Edges[n]);
+                T.connections.TryAdd(tile.id, n);
+            }
+        }
         private static void ParsingInputData(string filePath)
         {
             StreamReader file = new StreamReader(filePath);
             string[] Data = file.ReadToEnd().Split("\r\n\r\n");
 
             foreach(string s in Data)
-            {
-                Tile T = new Tile(s);
-                Tiles.Add(T);
-            }
+                Tiles.Add(new Tile(s));
         }
     }
 }
